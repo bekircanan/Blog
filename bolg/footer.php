d9f278ab988e361caf4a2d3d853e0316ee8f72bd
Canan <bekir.canan@etu.univ-lyon1.fr>
2024-10-14 21:42:17 +0200

    setup

---
+++ bolg/footer.php
@@ -0,0 +1,5 @@
+        <footer>
+            <p>&copy; <?php echo date("Y"); ?> Anime Blog.</p>
+        </footer>
+    </body>
+</html>
---
+++ bolg/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bolg</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header>
+        <div class="container">
+            <h1>Bolg</h1>
+            <nav>
+                <ul>
+                    <li><a href="login.php">Login</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
