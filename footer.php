9717679b35e566a290b08b18807a0d1cb460dd2e
SERIANI selsebil <selsebil.seriani@gmail.com>
2024-10-17 21:01:18 +0200

    - Ajout de la fonctionnalité de recherche (par pseudo, titre et catégorie) - Ajout l'affichage des résultats - Modification du CSS pour l'affichage des résultats

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
-        <footer>
-            <p>&copy; <?php echo date("Y"); ?> Anime Blog.</p>
-        </footer>
-    </body>
+        </body>
+    <footer>
+        <p>&copy; <?php echo date("Y"); ?> Anime Blog.</p>
+    </footer>
 </html>
