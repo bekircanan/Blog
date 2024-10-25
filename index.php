428a14b40c3109528cf53c46bcae0d2e05147644
Bekir <bekircanan2@gmail.com>
2024-10-25 08:48:31 +0200

    modifie sql index recherche

--- index.php
+++ index.php
@@ -9,13 +9,16 @@ require_once 'header.php';
 <?php
     if(isset($_GET['recherche'])){
         //requette sql pour chercher les articles par rapport au pseudo,titre et catégorie
-        $stmt = $conn->prepare("SELECT a.id_article, a.titre, a.date_pub, SUBSTR(a.contenu, 1, 150) as contenu, u.pseudo  
-                                FROM article a JOIN user u ON a.id_user = u.id_user 
-                                               JOIN article_categorie ac ON ac.id_article = a.id_article
-                                               JOIN categorie c ON ac.id_categorie = ac.id_categorie
+        $stmt = $conn->prepare("SELECT DISTINCT a.id_article, a.titre, a.date_pub, SUBSTR(a.contenu, 1, 150) as contenu, u.pseudo  
+                                FROM article a 
+                                        LEFT OUTER JOIN user u ON a.id_user = u.id_user 
+                                        LEFT OUTER JOIN article_categorie ac ON ac.id_article = a.id_article
+                                        LEFT OUTER JOIN categorie c ON ac.id_categorie = c.id_categorie
                                 WHERE u.pseudo LIKE :pseudo 
-                                OR a.titre LIKE :titre
-                                OR c.nom_categorie LIKE :categorie order by a.date_pub asc");
+                                        OR a.titre LIKE :titre
+                                        OR c.nom_categorie LIKE :categorie
+                                ORDER BY a.date_pub ASC;
+                                ");
         
         $stmt->execute([':pseudo' => '%' . $_GET['recherche']. '%',
                         ':titre' => '%' . $_GET['recherche']. '%',
